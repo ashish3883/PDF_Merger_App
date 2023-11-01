c63680f36925ffaae05bd0605b8fc8f4f1736d31
Ashish Kumar <ashishkumar3883@gmail.com>
2023-11-01 14:10:01 +0530

    input updation

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ app.get('/', (req, res) => {
 })
 
 app.post("/merge", upload.array('pdfs', 2), async (req, res, next)=> {
-    console.log(req.files)
     let d = await mergePdfs(path.join(__dirname, req.files[0].path), path.join(__dirname, req.files[1].path))
     res.redirect(`http://localhost:3000/static/${d}.pdf`)
     // req.files is array of `photos` files
